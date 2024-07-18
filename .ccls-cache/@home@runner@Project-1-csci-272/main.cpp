809a2bda396d6063f3aec144d9c2672705fba8bf
avros16 <114757828+avros16@users.noreply.github.com>
2024-07-18 14:28:10 +0000

    intro

--- .ccls-cache/@home@runner@Project-1-csci-272/main.cpp
+++ .ccls-cache/@home@runner@Project-1-csci-272/main.cpp
@@ -1,5 +1,56 @@
 #include <iostream>
+using namespace std; 
+
+void HarePos () {
+
+  
+}
+
+void RabbitPos() {
+  
+}
+
+
+
+
+
 
 int main() {
-  std::cout << "Hello World!\n";
+  
+string answer;
+string answer1;
+//int Hpos{0};
+//int rPos{0};
+  
+  
+cout << "Hello Welcome to the Tortoise and the Hare\n\n\n\n";
+cout << "The rules of the game: \n\n\n";
+    cout << "There are two characters: The Tortoise and the Hare. Each character has special moves. The Tortoise has: Fast plod, Slip, and Slow Plod. The Hare has: Sleep, big hop, big slip, small hop and small slip. \n\n\n There is no advantage to picking either charatacer. \n\n\n The first character to finish the race and reach square 70 will win the game. \n\n\n The reward for winning is  a pail of fresh carrots and lettuce. \n\n\n\nLets begin the game!!";
+cout << "\n\nAre you ready to play? (Y/N) ";
+  cin >> answer;
+
+  if (answer == "Y" || answer == "y") {
+
+
+
+
+
+cout << "\n\n\n\nBANG!!!!!! \n\n\n\nAND THEY ARE OFF!!!!!";
+
+
+
+
+
+
+
+
+
+
+    
+  } if (answer == "N" || answer == "n") {
+  
+cout << "inside the No";
+    }
+
+  
 }
